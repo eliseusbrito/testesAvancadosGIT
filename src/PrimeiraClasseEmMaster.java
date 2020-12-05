28563eed951488493d10bab98894ff46ef095d07
eliseusbrito <eliseusbrito@gmail.com>
2020-12-05 09:52:13 -0300

    Criado Primeira Classe em Master

---
+++ src/PrimeiraClasseEmMaster.java
@@ -0,0 +1,10 @@
+public class PrimeiraClasseEmMaster {
+
+    private int id;
+    private String Nome;
+
+    private void imprimeNome(){
+        System.out.println("Primeira Classe em Master");
+    }
+
+}
