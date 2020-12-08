4b6c0900baff3a74c987940b9f50460faa184ba7
eliseusbrito <eliseusbrito@gmail.com>
2020-12-08 07:57:55 -0300

    Adicionado segunda classe em master

---
+++ src/SegundaClasseEmMaster.java
@@ -0,0 +1,9 @@
+public class SegundaClasseEmMaster {
+
+    public void imprime(){
+        System.out.println("Criado segunda classe em master e tinha duas branchs" +
+                "(one e two) que foram criadas e commitadas" +
+                "mas não foi dado push");
+    }
+
+}
